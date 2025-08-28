49e44877e1de4c111364281f8a756c32086c2414
zaidkhan73 <zaid.s19khan@gmail.com>
2025-08-28 15:51:05 +0530

    clerk fixes

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   return <section className='flex items-center justify-center lg:min-h-[40vh]'>
     <div className="py-12 lg:py-24 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 lg:pt-12">
         <BgGradient className="from-rose-400 via-rose-300 to-orange-200"/>
-    <SignUp />
+    <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
     </div>
     </section>
 }
--- middleware.ts
+++ middleware.ts
@@ -9,7 +9,7 @@ const isProtectedRoute = createRouteMatcher([
 
 
 export default clerkMiddleware(async (auth, req)=>{
-  if (isProtectedRoute(req)) await auth.protect();
+  if (isProtectedRoute(req)) await auth.protect({});
 })
 
 export const config = {
