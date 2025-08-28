65a40f9a7b771ad7be626fce236f41eb6405b887
zaidkhan73 <zaid.s19khan@gmail.com>
2025-08-28 16:41:47 +0530

    seo optimisation

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,7 @@ import Header from "@/components/common/header";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
 import { ThemeProvider } from "@/components/theme/theme-provider";
-import { dark, neobrutalism, shadesOfPurple ,shadcn } from '@clerk/themes'
-
-
+import { dark, neobrutalism, shadesOfPurple, shadcn } from "@clerk/themes";
 
 const fontSans = FontSans({
   variable: "--font-sans",
@@ -17,6 +15,13 @@ const fontSans = FontSans({
 export const metadata: Metadata = {
   title: "ClariDoc",
   description: "AI powered PDF summariser",
+  openGraph: {
+    images: [
+      {
+        url: "/opengraph-image.png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -24,24 +29,17 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  
   return (
     <ClerkProvider
       appearance={{
-        theme: shadcn
+        theme: shadcn,
       }}
     >
       <html lang="en" suppressHydrationWarning>
-        <body
-          className={`${fontSans.variable} font-sans antialiased`}
-        >
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-          >
+        <body className={`${fontSans.variable} font-sans antialiased`}>
+          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
             <div className="p-12 flex items-center justify-end">
-              <Header/>
+              <Header />
             </div>
             <main>{children}</main>
             <Toaster />
